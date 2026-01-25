a7c1dfc3565a775c12761986282a127f712cef43
alicia <aliciafdezrov@gmail.com>
2026-01-25 19:55:49 +0100

    Added missing section to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Experience } from "@/pods/experience/experience.component";
 import { TechnicalSkills } from "@/pods/technical-skills/technical-skills.component";
 import { Background } from "@/components/background/background.component";
 import { UserProfile } from "@/pods/user-profile/user-profile.component";
+import { EducationAndCertifications } from "@/pods/education-and-certifications/education-and-certifications.component";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
           <AboutMe />
           <Experience />
           <TechnicalSkills />
+          <EducationAndCertifications />
         </div>
       </main>
     </div>
