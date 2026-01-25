ccf96176ab79c064bed7b035986de63d036f0924
alicia <aliciafdezrov@gmail.com>
2026-01-25 19:36:25 +0100

    Composed main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import { AboutMe } from "@/pods/about-me/about-me.component";
 import { Experience } from "@/pods/experience/experience.component";
 import { TechnicalSkills } from "@/pods/technical-skills/technical-skills.component";
+import { Background } from "@/components/background/background.component";
+import { UserProfile } from "@/pods/user-profile/user-profile.component";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Background />
       <main className={styles.main}>
-        <div className={styles.profile}>patata</div>
+        <UserProfile />
         <div className={styles.content}>
           <AboutMe />
           <Experience />
