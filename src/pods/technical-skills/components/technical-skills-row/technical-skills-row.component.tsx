f7fa2217a68d45f239dce9ce1577d47c6107c305
alicia <aliciafdezrov@gmail.com>
2026-01-25 19:35:00 +0100

    Applied tag component

--- src/pods/technical-skills/components/technical-skills-row/technical-skills-row.component.tsx
+++ src/pods/technical-skills/components/technical-skills-row/technical-skills-row.component.tsx
@@ -1,23 +1,26 @@
+import { Tag } from "@/components/tag/tag.component";
 import { TechnicalSkillsItem } from "../../technical-skills.vm";
 import styles from "./technical-skills-row.module.css";
 
 interface TechnicalSkillsRowProps {
   technicalSkillsRow: TechnicalSkillsItem;
 }
-export const TechnicalSkillsRowComponent = async (props: TechnicalSkillsRowProps) => {
+export const TechnicalSkillsRowComponent = async (
+  props: TechnicalSkillsRowProps
+) => {
   const { technicalSkillsRow } = props;
 
   return (
     <div className={styles.container}>
       <span className={styles.title}>{technicalSkillsRow.title}</span>
       {technicalSkillsRow.description && (
-        <span className={styles.description}>{technicalSkillsRow.description}</span>
+        <span className={styles.description}>
+          {technicalSkillsRow.description}
+        </span>
       )}
       <div className={styles.tagsContainer}>
         {technicalSkillsRow.skills.map((skill, index) => (
-          <div key={index} className={styles.tag}>
-            <span className={styles.tagLabel}>{skill}</span>
-          </div>
+          <Tag key={index} label={skill} />
         ))}
       </div>
     </div>
